105456a620a0a9d4f1df67fc53db54a1ba685120
Zhark10 <verygoodman18@yandex.ru>
2020-06-16 01:27:18 +0300

    added telegram bot api module

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const TelegramBot = require('node-telegram-bot-api')
+
 const TOKEN = '1203488910:AAFzoa784z8aAtHg17mn7iA4nfVum9AiMAk'
 
+const bot = new TelegramBot(TOKEN, {polling: true})
+
+bot.on('message', msg => {
+    bot.sendMessage(msg.chat.id, 'Hello!')
+})
