0a433af4d7ba5510577153fa5f8b78f84f519b17
Zhark10 <verygoodman18@yandex.ru>
2020-06-16 01:35:10 +0300

    for heroku

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const TOKEN = '1203488910:AAFzoa784z8aAtHg17mn7iA4nfVum9AiMAk'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, 'Hello!')
+    bot.sendMessage(msg.chat.id, `Hello, ${msg.from.first_name}!`)
 })
