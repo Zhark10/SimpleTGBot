0d69252971725027ca74fdb5c29c8f120a3b8faf
Zhark10 <verygoodman18@yandex.ru>
2020-06-17 01:33:49 +0300

    added unirest

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const TelegramBot = require('node-telegram-bot-api')
 
-const TOKEN = '1203488910:AAFzoa784z8aAtHg17mn7iA4nfVum9AiMAk'
+// const TOKEN = '1203488910:AAFzoa784z8aAtHg17mn7iA4nfVum9AiMAk'
 
-const bot = new TelegramBot(TOKEN, {polling: true})
+// const bot = new TelegramBot(TOKEN, {polling: true})
 
-bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, `Hello, ${msg.from.first_name}!`)
-})
+// bot.on('message', msg => {
+//     bot.sendMessage(msg.chat.id, `Hello, ${msg.from.first_name}!`)
+// })
+
+
+var unirest = require("unirest");
+
+var req = unirest("POST", "https://financialtimesmikilior1v1.p.rapidapi.com/getAspectsList");
+
+req.headers({
+	"x-rapidapi-host": "FinancialTimesmikilior1V1.p.rapidapi.com",
+	"x-rapidapi-key": "29e0404db1msh66a945e13f17ff9p10aadfjsn27f47733b676",
+	"content-type": "application/x-www-form-urlencoded",
+	"useQueryString": true
+});
+
+req.form({});
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
